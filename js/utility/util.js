a59840600492394b073d03a62b498829b8b59fe5
Andrew Grachov <DrontXL@yandex.ru>
2014-06-04 11:53:46 +0300

    util update

--- js/utility/util.js
+++ js/utility/util.js
@@ -1,51 +1,166 @@
-(function (){
-	var checkArrayPresence = function(doc,key,modifier) {
-
-	var filtered = doc[key].filter(function(item){
+(function () {
+var checkArrayPresence = function (doc, key, modifier) {
+	var filtered = doc[key].filter(function (item) {
 		return item == modifier[key];
 	});
 
 	return filtered.length > 0;
 };
 
+function deepClone(object) {
+	return JSON.parse(JSON.stringify(object));
+}
+
+function prop(name) {
+	var propertiesArray = name.split('.');
+	return function (object) {
+		if (!object) {
+			return;
+		}
+		var property = object;
+		for (var i = 0; i < propertiesArray.length; i++) {
+			if (typeof propertiesArray[i + 1] === 'number' && propertiesArray[i] instanceof Array) {
+				property = property[propertiesArray[i]][propertiesArray[i + 1]];
+				i++;
+			} else {
+				property = property[propertiesArray[i]];
+
+				if (typeof(property) === 'undefined') {
+					return;
+				}
+
+				if (property === null) {
+					if (i + 1 === propertiesArray.length) {
+						return null;
+					} else {
+						return;
+					}
+				}
+			}
+		}
+		return property;
+	};
+}
+
+function setProp(name) {
+	var propertiesArray = name.split('.');
+	return function (object, value) {
+		if (!object) {
+			return;
+		}
+		var property = object;
+		for (var i = 0; i < propertiesArray.length - 1; i++) {
+			if (typeof(property[propertiesArray[i]]) === 'undefined') {
+				property[propertiesArray[i]] = {};
+			}
+			property = property[propertiesArray[i]];
+		}
+		property[propertiesArray[i]] = value;
+		return value;
+	};
+}
+
+function addPropIfNotExist(obj, prop, value) {
+	if (obj instanceof Object && !(prop in obj)) {
+		obj[prop] = value;
+	}
+	return obj;
+}
+
+function renameProp(obj, oldProp, newProp) {
+	var oldPropertyValue = prop(oldProp)(obj);
+	setProp(newProp)(obj, oldPropertyValue);
+	deleteProp(oldProp)(obj);
+	return obj;
+}
+
+function deleteProp(obj, oldProp) {
+	setProp(oldProp)(obj, void 0);
+	return obj;
+}
+
+function addArrayItemsPropIfNotExist(arr, prop, value) {
+	if (arr instanceof Array) {
+		arr.forEach(function (item) {
+			addPropIfNotExist(item, prop, value);
+		});
+	}
+	return arr;
+}
+
+function renameArrayItemsProp(arr, oldProp, newProp) {
+	if (arr instanceof Array) {
+		arr.forEach(function (item) {
+			renameProp(item, oldProp, newProp);
+		});
+	}
+	return arr;
+}
+
+function deleteArrayItemsProp(arr, oldProp) {
+	if (arr instanceof Array) {
+		arr.forEach(function (item) {
+			deleteProp(item, oldProp);
+		});
+	}
+	return arr;
+}
+
+var objectUtil = {
+	deepClone: deepClone,
+	prop: prop,
+	setProp: setProp,
+	addPropIfNotExist: addPropIfNotExist,
+	renameProp: renameProp,
+	deleteProp: deleteProp,
+	addArrayItemsPropIfNotExist: addArrayItemsPropIfNotExist,
+	renameArrayItemsProp: renameArrayItemsProp,
+	deleteArrayItemsProp: deleteArrayItemsProp
+};
+
+
+
+
 var modifiers = {
-	'$set': function(doc,modifier) {
-		Object.keys(modifier).forEach(function(key) {
-			doc[key] = modifier[key];
+	'$set': function (doc, modifier) {
+		Object.keys(modifier).forEach(function (key) {
+			objectUtil.setProp(key)(doc, modifier[key]);
 		});
 		return doc;
 	},
 
-	'$unset': function(doc,modifier) {
-		Object.keys(modifier).forEach(function(key) {
-			delete doc[key];
+	'$unset': function (doc, modifier) {
+		Object.keys(modifier).forEach(function (key) {
+			objectUtil.deleteProp(doc, key);
 		});
 		return doc;
 	},
 
-	'$addToSet': function(doc,modifier) {
-		Object.keys(modifier).forEach(function(key) {
-			if(!doc[key] instanceof Array){
-				throw '$addToSet called on non array property'+key;
+	'$addToSet': function (doc, modifier) {
+		Object.keys(modifier).forEach(function (key) {
+			var docProperty = objectUtil.prop(key)(doc);
+			if (!docProperty instanceof Array) {
+				throw '$addToSet called on non array property' + key;
 			}
 			var isPresent;
+			var modifierProperty = modifier[key];
 
-			if(modifier[key].$each && modifier[key].$each instanceof Array) {
-				modifier[key].$each.forEach(function (modifierItem) {
+			if (modifierProperty.$each && modifierProperty.$each instanceof Array) {
+				modifierProperty.$each.forEach(function (modifierItem) {
 
-					var mod= {};
-					mod[key] = modifierItem;
-					isPresent = checkArrayPresence(doc,key,mod);
+					var mod = {};
+					objectUtil.setProp(key)(mod, modifierItem);
+					isPresent = checkArrayPresence(doc, key, mod);
 
 					if (!isPresent) {
-						doc[key].push(modifierItem);
+						docProperty.push(modifierItem);
 					}
 				});
 			}
 			else {
-				isPresent = checkArrayPresence(doc,key,modifier);
+				isPresent = checkArrayPresence(doc, key, modifier);
 				if (!isPresent) {
-					doc[key].push(modifier[key]);
+					docProperty.push(modifierProperty);
 				}
 			}
 		});
@@ -53,69 +168,78 @@ var modifiers = {
 
 	},
 
-	'$inc': function(doc, modifier) {
-		Object.keys(modifier).forEach(function(key) {
-			doc[key]+=modifier[key];
+	'$inc': function (doc, modifier) {
+		Object.keys(modifier).forEach(function (key) {
+			var docProp = objectUtil.prop(key)(doc);
+			var modifierValue = modifier[key];
+			docProp += modifierValue;
 		});
 		return doc;
 	},
 
-	'$rename': function(doc, modifier) {
-		Object.keys(modifier).forEach(function(key) {
-			doc[modifier[key]] = doc[key];
-			delete doc[key];
+	'$rename': function (doc, modifier) {
+		Object.keys(modifier).forEach(function (key) {
+			var newPropertyKey = objectUtil.prop(key)(modifier);
+			objectUtil.renameProp(doc, key, newPropertyKey);
 		});
 		return doc;
 	},
 
-	$pop: function(doc, modifier) {
-		Object.keys(modifier).forEach(function(key) {
-			if (!doc[key] instanceof Array) {
+	$pop: function (doc, modifier) {
+		Object.keys(modifier).forEach(function (key) {
+			var property = objectUtil.prop(key)(doc);
+			var modifierValue = modifier[key];
+
+			if (!property instanceof Array) {
 				throw "property " + key + " is not array";
 			}
 
-			if (modifier[key] == 1) {
-				doc[key].pop();
+			if (modifierValue == 1) {
+				property.pop();
 			}
 
-			else if (modifier[key] == -1) {
-				doc[key].shift();
+			else if (modifierValue == -1) {
+				property.shift();
 			}
 		});
 
 		return doc;
 	},
 
-	$push: function(doc, modifier) {
-		Object.keys(modifier).forEach(function(key) {
-			if (!doc[key] instanceof Array) {
+	$push: function (doc, modifier) {
+		Object.keys(modifier).forEach(function (key) {
+			var property = objectUtil.prop(key)(doc);
+			var modifierProperty = modifier[key];
+
+			if (!property instanceof Array) {
 				throw "property " + key + " is not array";
 			}
 
-			if(modifier[key].$each && modifier[key].$each instanceof Array) {
-				modifier[key].$each.forEach(function (modifierItem) {
-
-					doc[key].push(modifierItem);
+			if (modifierProperty.$each && modifierProperty.$each instanceof Array) {
+				modifierProperty.$each.forEach(function (modifierItem) {
+					property.push(modifierItem);
 				});
 			}
 			else {
-				doc[key].push(modifier[key]);
+				property.push(modifierProperty);
 			}
 		});
 
 		return doc;
 	},
 
-	$pull: function(doc,modifier) {
+	$pull: function (doc, modifier) {
 		Object.keys(modifier).forEach(function (key) {
+			var property = objectUtil.prop(key)(doc);
+			var modifierProperty = modifier[key];
 
-			if (!doc[key] instanceof Array) {
+			if (!property instanceof Array) {
 				throw "property " + key + "is not array";
 			}
 
-			var subCollection = new Wrap(doc[key].slice());
-			subCollection.remove(modifier[key]); //todo: UGLY AS SHIT
-			doc[key] = subCollection._data;
+			var subCollection = new Wrap(property.slice());
+			subCollection.remove(modifierProperty); //todo: UGLY AS SHIT
+			objectUtil.setProp(key)(doc, subCollection._data);
 		});
 
 		return doc;
@@ -123,63 +247,72 @@ var modifiers = {
 };
 
 var comparsionOperators = {
-	'$gt': function(doc, field, value) {
-		return doc[field]>value;
+	'$gt': function (doc, field, value) {
+		var property = objectUtil.prop(field)(doc);
+		return property > value;
 	},
-	'$gte': function(doc, field, value) {
-		return doc[field]>=value;
+	'$gte': function (doc, field, value) {
+		var property = objectUtil.prop(field)(doc);
+		return property >= value;
 	},
-	'$lt': function(doc, field, value) {
-		return doc[field]<value;
+	'$lt': function (doc, field, value) {
+		var property = objectUtil.prop(field)(doc);
+		return property < value;
 	},
-	'$lte': function(doc, field, value) {
-		return doc[field]<=value;
+	'$lte': function (doc, field, value) {
+		var property = objectUtil.prop(field)(doc);
+		return property <= value;
 	},
 
-	'$in': function(doc, field, value) {
-		if(!value instanceof Array){
+	'$in': function (doc, field, value) {
+		var property = objectUtil.prop(field)(doc);
+		if (!value instanceof Array) {
 			throw 'should set an array for $in call';
 		}
-		if(doc[field] instanceof Array){
-			return doc[field].some(function(docFieldItem) {
-				return value.some(function(valueFieldItem) {
+		if (property instanceof Array) {
+			return property.some(function (docFieldItem) {
+				return value.some(function (valueFieldItem) {
 					return docFieldItem == valueFieldItem;
 				});
 			});
 		}
-		else{
-			return value.some(function(valueItem) {
-				return doc[field] == valueItem;
+		else {
+			return value.some(function (valueItem) {
+				return property == valueItem;
 			});
 		}
 	},
-	$nin: function(doc, field, value) {
+	$nin: function (doc, field, value) {
 		return !this.$in(doc, field, value);
 	},
-	'$regex': function(doc, field, value) {
-		return value.test(doc[field]);
+	'$regex': function (doc, field, value) {
+		var property = objectUtil.prop(field)(doc);
+		return value.test(property);
 	},
 
-	'$ne': function(doc, field, value) {
-
-		return doc[field] != value;
+	'$ne': function (doc, field, value) {
+		var property = objectUtil.prop(field)(doc);
+		return property != value;
 	},
 
-	'$exists': function(doc, field, value) {
-		return (doc[field] !== undefined) === value; //null values does not include
+	'$exists': function (doc, field, value) {
+		var property = objectUtil.prop(field)(doc);
+		return (property !== undefined) === value; //null values does not include
 	},
 
-	$size: function(doc, field, value) {
-		if (doc[field] && doc[field].length ) {
-			return doc[field].length === value;
+	$size: function (doc, field, value) {
+		var property = objectUtil.prop(field)(doc);
+		if (property && property.length) {
+			return property.length === value;
 		}
 		return false;
 	},
 
-	$all: function(doc, field, value) {
-		if (doc[field]) {
+	$all: function (doc, field, value) {
+		var property = objectUtil.prop(field)(doc);
+		if (property) {
 			return value.every(function (valueItem) {
-				doc[field].some(function (docFieldItem) {
+				property.some(function (docFieldItem) {
 					docFieldItem == valueItem;
 				});
 			});
@@ -187,10 +320,11 @@ var comparsionOperators = {
 		return false;
 	},
 
-	$elemMatch: function(doc, field, condition) {
-		if(doc[field]){
-		return doc[field].some(function (element) {
-			return _allKeysValid(condition,element);
+	$elemMatch: function (doc, field, condition) {
+		var property = objectUtil.prop(field)(doc);
+		if (property) {
+			return property.some(function (element) {
+				return _allKeysValid(condition, element);
 			});
 		}
 		return false;
@@ -200,41 +334,50 @@ var comparsionOperators = {
 };
 
 var logicalOperators = {
-	$or: function(doc,array) {
+	$or: function (doc, array) {
 		return array.some(function (condition) {
-			return _allKeysValid(condition,doc);
+			return _allKeysValid(condition, doc);
 		});
 	},
 
-	$and: function(doc,array) {
+	$and: function (doc, array) {
 		return array.every(function (condition) {
-			return _allKeysValid(condition,doc);
+			return _allKeysValid(condition, doc);
 		});
 	},
 
-	$not: function(doc, condition) {
-		return !_allKeysValid(condition,doc);
+	$not: function (doc, condition) {
+		return !_allKeysValid(condition, doc);
 	},
 
-	$nor: function(doc,array) {
-		return !this.$and(doc,array);
+	$nor: function (doc, array) {
+		return !this.$and(doc, array);
 	}
 
 };
 //todo:wtf
-function areEqual (field1,field2) {
-
+function areEqual(field1, field2) {
 	return field1 == field2;
 }
 
-function _updateDoc(doc,modifier,wrapped) {
+function _updateDoc(doc, modifier, wrapped) {
+	var isUpdatingWholeDoc = Object.keys(modifier).every(function (key) {
+		return !modifiers[key];
+	});
 
-	Object.keys(modifier).forEach(function(key) {
-		if(!modifiers[key]){
-			throw 'this modifier is not supported for now or invalid: '+ key;
+	if (isUpdatingWholeDoc) {
+		var index = wrapped._data.indexOf(doc);
+		wrapped._data[index] = doc;
+		return;
+	}
+
+	Object.keys(modifier).forEach(function (key) {
+		if (!modifiers[key]) {
+			throw 'this modifier is not supported for now or invalid: ' + key;
 		}
 		else {
-			var modifiedDoc = modifiers[key](doc,modifier[key]);
+			var modifierValue = modifier[key];
+			var modifiedDoc = modifiers[key](doc, modifierValue);
 			var index = wrapped._data.indexOf(doc);
 			wrapped._data[index] = modifiedDoc;
 		}
@@ -252,52 +395,59 @@ function Constructor(array) {
 	return new Wrap(array);
 }
 
-function _allKeysValid(query,item) {
+function _allKeysValid(query, item) {
 
-	if(typeof query !== 'object') {
+	if (typeof query !== 'object') {
 		return query === item;
 	}
 
-	if(!query || !item) {
+	if (!query || !item) {
 		return false;
 	}
 
-	return Object.keys(query).every(function(key) {
-		if (logicalOperators[key]){
-			return logicalOperators[key](item,query[key]);
+	function anyMatchOf(key, pathStep, item) {
+		return item[pathStep].some(function (subArrayItem) {
+			var subQueryKey = key.replace(new RegExp(pathStep + '\\.'), "");
+			var subQuery = {};
+			subQuery[subQueryKey] = query[key];
+
+			return _allKeysValid(subQuery, subArrayItem);
+		});
+	}
+
+
+	return Object.keys(query).every(function (key) {
+		if (logicalOperators[key]) {
+			return logicalOperators[key](item, query[key]);
 		}
 		// price:{gte:32}
-		else if(typeof query[key]=='object') {
-			return Object.keys(query[key]).every(function(operator) {
-				return comparsionOperators[operator](item,key,query[key][operator]);
+		else if (typeof query[key] == 'object') {
+			return Object.keys(query[key]).every(function (operator) {
+				return comparsionOperators[operator](item, key, query[key][operator]);
 			});
 		}
-		else if (typeof key === 'string' && new RegExp('.*\..*').test(key)) {
+		else if (typeof key === 'string' && new RegExp('.*\\..*').test(key)) {
 			var properties = key.split('.');
 			var path = [];
-			for (i = 0; i < properties.length; i++){
+
+
+			for (var i = 0; i < properties.length; i++) {
 				var property = properties[i];
 				path.push(property);
 				var pathStep = path.join(".");
 				if (item[pathStep] instanceof Array) {
-					var nextOperator = parseInt(properties[i+1]);
+					var nextOperator = parseInt(properties[i + 1]);
 					if (!isNaN(nextOperator)) {
 
-						var subQueryKey = key.replace(new RegExp(pathStep+'\.'+properties[i+1]+'\.'),"");
+						var subQueryKey = key.replace(new RegExp(pathStep + '\\.' + properties[i + 1] + '\\.'), "");
 						var subQuery = {};
 						subQuery[subQueryKey] = query[key];
-						var subArrayItem = item[pathStep][properties[i+1]];
+						var subArrayItem = item[pathStep][properties[i + 1]];
 
-						return _allKeysValid(subQuery,subArrayItem);
+						return _allKeysValid(subQuery, subArrayItem);
 					} else {
-
-						return item[pathStep].some( function (subArrayItem) {
-							var subQueryKey = key.replace(new RegExp(pathStep+'\.'),"");
-							var subQuery = {};
-							subQuery[subQueryKey] = query[key];
-
-							return _allKeysValid(subQuery,subArrayItem);
-						});
+						//key,pathStepitem
+						return anyMatchOf(key, pathStep, item);
 					}
 				}
 			}
@@ -307,16 +457,16 @@ function _allKeysValid(query,item) {
 	});
 }
 
-Wrap.prototype.find = function(query,options) {
-	query = query || {};
+Wrap.prototype.find = function (query, options) {
+
 	options = options || {};
 
-	var filteredArray = this._data.filter(function(item){
-		return _allKeysValid(query,item);
+	var filteredArray = this._data.filter(function (item) {
+		return _allKeysValid(query, item);
 	});
 
 	if (options.skip) {
-		filteredArray = filteredArray.slice(options.skip,filteredArray.length);
+		filteredArray = filteredArray.slice(options.skip, filteredArray.length);
 	}
 
 	if (options.limit) {
@@ -326,37 +476,37 @@ Wrap.prototype.find = function(query,options) {
 	return filteredArray;
 };
 
-Wrap.prototype.findOne = function(query) {
-	for (var i=0; i<this._data.length; i++) {
-		if (_allKeysValid(query,this._data[i])) {
+Wrap.prototype.findOne = function (query) {
+	for (var i = 0; i < this._data.length; i++) {
+		if (_allKeysValid(query, this._data[i])) {
 			return this._data[i];
 		}
 	}
 	return;
 };
 
-Wrap.prototype.update = function (query,modifier,options) {
+Wrap.prototype.update = function (query, modifier, options) {
 	options = options || {};
 
 	var self = this;
 	var counter = 0;
 
 	if (options.multi) {
-		var docs = this.find(query,{});
-		docs.forEach(function(doc) {
-			_updateDoc(doc,modifier,self);
-				counter++;
+		var docs = this.find(query, {});
+		docs.forEach(function (doc) {
+			_updateDoc(doc, modifier, self);
+			counter++;
 		});
 	}
 	else {
 		var doc = this.findOne(query);
 		if (doc) {
 			counter++;
-			_updateDoc(doc,modifier,self);
+			_updateDoc(doc, modifier, self);
 		}
 	}
-	if(counter === 0 && options.upsert) {
-		if(modifier.$set){
+	if (counter === 0 && options.upsert) {
+		if (modifier.$set) {
 			counter++;
 			this.insert(modifier.$set);
 		}
@@ -373,24 +523,32 @@ Wrap.prototype.remove = function (query) {
 	var self = this;
 	var docs = this.find(query);
 
-	for(var i =0; i<docs.length; i++){
+	for (var i = 0; i < docs.length; i++) {
 		var doc = docs[i];
 		var index = self._data.indexOf(doc);
 
-		if(index !== -1) {
-			self._data.splice(index,1);
+		if (index !== -1) {
+			self._data.splice(index, 1);
 		}
 	}
 
 	return;
 };
 
-Wrap.prototype.findAndModify = function (query,modifier,options) {
+Wrap.prototype.findAndModify = function (query, modifier, options) {
 	options = options || {};
 
 	var doc = this.findOne(query);
 	if (doc) {
-		_updateDoc(doc,modifier,this);
+		if (options.new) {
+			_updateDoc(doc, modifier, this);
+		}
+		else {
+			_updateDoc(JSON.parse(JSON.stringify(doc)), modifier, this);
+		}
+	}
+	else if (!modifier.$set && options.upsert) {
+		doc = this.insert(modifier);
 	}
 	return doc;
 };
